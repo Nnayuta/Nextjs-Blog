96bd97ce74cabd67c3d5be55fdb247b52f6e70d5
Nayuta <naayudan@gmail.com>
2022-06-14 23:25:24 -0300

    testando isso so pra ver no que vai da

--- src/providers/user-Provider.ts
+++ src/providers/user-Provider.ts
@@ -3,13 +3,17 @@ import { User } from "../models/User";
 const user = {
     uuid: "admin",
     name: 'Nayuta',
-    email: 'nayuta@nizzy.com',
-    password: 'admin',
+    email: 'nayuta@email.com',
+    password: 'SenhaPoderosa',
     avatar: 'https://www.artmajeur.com/medias/standard/a/d/adamsdexter/artwork/10493311_anime-girl-edit-kawaii-manga-cap-favim-com-2608292.jpg',
 } as User;
 
 export async function findByEmailAndPassword(email: string, password: string): Promise<User> {
-    return user;
+
+    if(email == 'nayuta@email.com' && password =='SenhaPoderosa'){
+        return user;
+    }
+    return null;
 }
 
 export function recoveryUserByToken(token: string) {

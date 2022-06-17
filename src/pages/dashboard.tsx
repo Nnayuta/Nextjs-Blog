cfd4a0485623ca42bce02cee9560bf53a22d55f9
Nayuta <naayudan@gmail.com>
2022-06-17 08:19:57 -0300

    aaaaaaaaaaaa

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,22 @@
 import { useContext } from "react";
 import LayoutDashboard from "../components/Dashboard/LayoutDashboard";
 import LoginArea from "../components/Dashboard/Login";
-import * as db from '../providers/Posts-Provider'
 import { AuthContext } from "../contexts/AuthContext";
+import * as db from '../providers/Posts-Provider';
+
+export async function getServerSideProps() {
+  const posts = await db.getAllPosts();
+  
+  const resMul = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/files/upload`);
+  const multimidia = await resMul.json();
+
+  return {
+    props: {
+      posts,
+      multimidia
+    },
+  }
+}
 
 export default function Dashboard({ posts, multimidia }) {
   const { user } = useContext(AuthContext);
@@ -20,18 +34,4 @@ export default function Dashboard({ posts, multimidia }) {
       }
     </>
   )
-}
-
-export async function getServerSideProps() {
-  const posts = await db.getAllPosts();
-  
-  const resMul = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/files/upload`);
-  const multimidia = await resMul.json();
-
-  return {
-    props: {
-      posts,
-      multimidia
-    },
-  }
 }
